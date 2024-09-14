fd97a6f944199afc95b38945e9c3f143f3ad75b4
t.a.mirfin@gmail.com <t.a.mirfin@gmail.com>
2024-09-14 19:57:44 +0100

    updating app.listen

--- dist/app.js
+++ dist/app.js
@@ -30,11 +30,11 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
-database_1.default.query('SELECT * FROM users').then(([rows]) => { console.log(rows); });
+// db.query('SELECT * FROM users').then(([rows]) => { console.log(rows) });
 // Start the server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(Number(PORT), '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 // Properly close the database connection when needed
 process.on('SIGINT', () => {
     database_1.default.end().then(() => {
